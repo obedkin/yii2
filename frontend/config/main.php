130e1a8576c09393a9c27730a43f8f4dd77d3d7f
obedkin <obedkin@gmail.com>
2015-06-07 19:59:49 +0300

    Урок-01. Установка Yii2. Настройка ЧПУ.

---
+++ frontend/config/main.php
@@ -0,0 +1,47 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-frontend',
+    'basePath' => dirname(__DIR__),
+    'bootstrap' => ['log'],
+    'controllerNamespace' => 'frontend\controllers',
+    'components' => [
+      'mail' => [
+        'class'            => 'zyx\phpmailer\Mailer',
+        'viewPath'         => '@common/mail',
+        'useFileTransport' => false,
+        'config'           => [
+          'mailer'     => 'smtp',
+          'host'       => 'smtp.yandex.ru',
+          'port'       => '465',
+          'smtpsecure' => 'ssl',
+          'smtpauth'   => true,
+          'username'   => '',
+          'password'   => '',
+        ],
+      ],
+      'user' => [
+            'identityClass' => 'common\models\User',
+            'enableAutoLogin' => true,
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+    ],
+    'params' => $params,
+];
