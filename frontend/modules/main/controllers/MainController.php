ec57692ec1873a3c4f8f3d8f14a55c8a3242f71c
obedkin <obedkin@gmail.com>
2015-06-28 00:13:36 +0300

    Урок-03. Создание формы регистрации

--- frontend/modules/main/controllers/MainController.php
+++ frontend/modules/main/controllers/MainController.php
@@ -2,13 +2,33 @@
 namespace app\modules\main\controllers;
 
 use frontend\models\Image;
+use frontend\models\SignupForm;
+use yii\base\Response;
+use yii\bootstrap\ActiveForm;
 
 class MainController extends \yii\web\Controller
 {
+    public $layout = 'inner';
+
     public function actionIndex()
     {
-        $this->layout = 'bootstrap';
         return $this->render('index');
+    }
+
+    public function actionRegister()
+    {
+        $model = new SignupForm();
+
+        if(\Yii::$app->request->isAjax && \Yii::$app->request->isPost){
+            \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+            return ActiveForm::validate($model);
+        }
+
+        if ($model->load($_POST) && $model->signup()){
+            print_r ($model->getAttributes());
+            die;
+        }
+        return $this->render('register',['model'=>$model]);
     }
 
 }
