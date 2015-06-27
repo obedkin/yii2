5f126c60926862be7f666c8315a6769156351e8e
obedkin <obedkin@gmail.com>
2015-06-27 16:49:14 +0300

    Урок-02. Установка phpmailer

--- common/config/main.php
+++ common/config/main.php
@@ -1,6 +1,12 @@
 <?php
 return [
+  'name'=> 'Study Yii2',
   'vendorPath' => dirname(dirname(__DIR__)) . '/vendor',
+  'modules'=>[
+    'main'=>[
+      'class'=>'app\modules\main\Module'
+    ]
+  ],
   'components' => [
     'cache' => [
       'class' => 'yii\caching\FileCache',
--- frontend/config/main.php
+++ frontend/config/main.php
@@ -1,47 +1,49 @@
 <?php
-$params = array_merge(
-    require(__DIR__ . '/../../common/config/params.php'),
-    require(__DIR__ . '/../../common/config/params-local.php'),
-    require(__DIR__ . '/params.php'),
-    require(__DIR__ . '/params-local.php')
-);
+$params = array_merge(require(__DIR__ . '/../../common/config/params.php'),
+  require(__DIR__ . '/../../common/config/params-local.php'),
+  require(__DIR__ . '/params.php'), require(__DIR__ . '/params-local.php'));
 
 return [
-    'id' => 'app-frontend',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
-    'controllerNamespace' => 'frontend\controllers',
-    'components' => [
-      'mail' => [
-        'class'            => 'zyx\phpmailer\Mailer',
-        'viewPath'         => '@common/mail',
-        'useFileTransport' => false,
-        'config'           => [
-          'mailer'     => 'smtp',
-          'host'       => 'smtp.yandex.ru',
-          'port'       => '465',
-          'smtpsecure' => 'ssl',
-          'smtpauth'   => true,
-          'username'   => '',
-          'password'   => '',
+  'id' => 'app-frontend',
+  'basePath' => dirname(__DIR__),
+  'bootstrap' => ['log'],
+  'controllerNamespace' => 'frontend\controllers',
+  'modules' => [
+    'main' => [
+      'class' => 'app\modules\main\Module'
+    ]
+  ],
+  'components' => [
+    'mail' => [
+      'class' => 'zyx\phpmailer\Mailer',
+      'viewPath' => '@common/mail',
+      'useFileTransport' => FALSE,
+      'config' => [
+        'mailer' => 'smtp',
+        'host' => 'smtp.yandex.ru',
+        'port' => '465',
+        'smtpsecure' => 'ssl',
+        'smtpauth' => TRUE,
+        'username' => '',
+        'password' => '',
+      ],
+    ],
+    'user' => [
+      'identityClass' => 'common\models\User',
+      'enableAutoLogin' => TRUE,
+    ],
+    'log' => [
+      'traceLevel' => YII_DEBUG ? 3 : 0,
+      'targets' => [
+        [
+          'class' => 'yii\log\FileTarget',
+          'levels' => ['error', 'warning'],
         ],
       ],
-      'user' => [
-            'identityClass' => 'common\models\User',
-            'enableAutoLogin' => true,
-        ],
-        'log' => [
-            'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
-                [
-                    'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
-                ],
-            ],
-        ],
-        'errorHandler' => [
-            'errorAction' => 'site/error',
-        ],
     ],
-    'params' => $params,
+    'errorHandler' => [
+      'errorAction' => 'site/error',
+    ],
+  ],
+  'params' => $params,
 ];
