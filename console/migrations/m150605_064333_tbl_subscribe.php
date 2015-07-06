11ed0c5eae6f53a5a679c1bad6bc19e63b151258
obedkin <obedkin@gmail.com>
2015-07-06 22:00:43 +0300

    Урок-04/2. Модели advert и subscribe

---
+++ console/migrations/m150605_064333_tbl_subscribe.php
@@ -0,0 +1,39 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m150605_064333_tbl_subscribe extends Migration
+{
+    public function up()
+    {
+
+        $this->execute("
+        CREATE TABLE IF NOT EXISTS `subscribe` (
+  `idsubscribe` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) DEFAULT NULL,
+  `date_subscribe` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`idsubscribe`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+        ");
+
+    }
+
+    public function down()
+    {
+       $this->dropTable("subscribe");
+
+        return false;
+    }
+    
+    /*
+    // Use safeUp/safeDown to run migration code within a transaction
+    public function safeUp()
+    {
+    }
+    
+    public function safeDown()
+    {
+    }
+    */
+}
