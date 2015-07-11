2ca54f5843bc6038c5cf5370bc7e9c3566c0e2ab
obedkin <obedkin@gmail.com>
2015-07-11 20:03:01 +0300

    Урок-06. Добавляем еще 2 дополнительных слайдера на главную страницу

--- frontend/components/Common.php
+++ frontend/components/Common.php
@@ -2,6 +2,7 @@
 namespace frontend\components;
 
 use yii\base\Component;
+use yii\helpers\BaseFileHelper;
 use yii\helpers\Url;
 
 class Common extends Component{
@@ -10,11 +11,11 @@ class Common extends Component{
 
     public function sendMail($subject,$text,$emailFrom='optimus58@mail.ru',$nameFrom='Advert'){
         if(\Yii::$app->mail->compose()
-            ->setFrom(['yii2.school@yandex.ru' => 'Advert'])
-            ->setTo([$emailFrom => $nameFrom])
-            ->setSubject($subject)
-            ->setHtmlBody($text)
-            ->send()){
+          ->setFrom(['yii2.school@yandex.ru' => 'Advert'])
+          ->setTo([$emailFrom => $nameFrom])
+          ->setSubject($subject)
+          ->setHtmlBody($text)
+          ->send()){
             $this->trigger(self::EVENT_NOTIFY);
             return true;
         }
@@ -33,13 +34,20 @@ class Common extends Component{
     public static function getImageAdvert($data,$general = true,$original = false){
 
         $image = [];
-        $base = Url::base();
-        if($original){
+        $base = '/';
+        if($general){
 
-            $image[] = $base.'uploads/adverts/'.$data['idadvert'].'/general/'.$data['general_image'];
+            $image[] = $base.'uploads/adverts/'.$data['idadvert'].'/general/small_'.$data['general_image'];
         }
         else{
-            $image[] = $base.'uploads/adverts/'.$data['idadvert'].'/general/small_'.$data['general_image'];
+            $path = \Yii::getAlias("@frontend/web/uploads/adverts/".$data['idadvert']);
+            $files = BaseFileHelper::findFiles($path);
+
+            foreach($files as $file){
+                if (strstr($file, "small_") && !strstr($file, "general")) {
+                    $image[] = $base . 'uploads/adverts/' . $data['idadvert'] . '/' . basename($file);
+                }
+            }
         }
 
         return $image;
@@ -48,6 +56,11 @@ class Common extends Component{
     public static function substr($text,$start=0,$end=50){
 
         return mb_substr($text,$start,$end);
+    }
+
+
+    public static function getType($row){
+        return ($row['sold']) ? 'Sold' : 'New';
     }
 
 
