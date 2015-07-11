0eff27ca2d75e1cb3e5568e939f4a991a136f5ac
obedkin <obedkin@gmail.com>
2015-07-11 21:52:20 +0300

    Урок-06. Страница с конкретной квартирой

--- frontend/modules/main/controllers/MainController.php
+++ frontend/modules/main/controllers/MainController.php
@@ -2,10 +2,12 @@
 
 namespace app\modules\main\controllers;
 
+use common\models\Advert;
 use common\models\LoginForm;
 use frontend\models\ContactForm;
 use frontend\models\Image;
 use frontend\models\SignupForm;
+use yii\base\DynamicModel;
 use yii\web\Response;
 use yii\widgets\ActiveForm;
 
@@ -81,6 +83,47 @@ class MainController extends \yii\web\Controller
         }
 
         return $this->render("contact", ['model' => $model]);
+    }
+
+    public function actionViewAdvert($id){
+        $model = Advert::findOne($id);
+
+        $data = ['name', 'email', 'text'];
+        $model_feedback = new DynamicModel($data);
+        $model_feedback->addRule('name','required');
+        $model_feedback->addRule('email','required');
+        $model_feedback->addRule('text','required');
+        $model_feedback->addRule('email','email');
+
+
+        if(\Yii::$app->request->isPost) {
+            if ($model_feedback->load(\Yii::$app->request->post()) && $model_feedback->validate()){
+
+                \Yii::$app->common->sendMail('Subject Advert',$model_feedback->text);
+            }
+
+        }
+
+        $user = $model->user;
+        $images = \frontend\components\Common::getImageAdvert($model,false);
+
+        $current_user = ['email' => '', 'username' => ''];
+
+        if(!\Yii::$app->user->isGuest){
+
+            $current_user['email'] = \Yii::$app->user->identity->email;
+            $current_user['username'] = \Yii::$app->user->identity->username;
+
+        }
+
+        return $this->render('view_advert',[
+          'model' => $model,
+          'model_feedback' => $model_feedback,
+          'user' => $user,
+          'images' =>$images,
+          'current_user' => $current_user
+        ]);
+
     }
 
 }
